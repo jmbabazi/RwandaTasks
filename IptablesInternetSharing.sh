8cbf1415203c5b75157d835da317fa57834e873d
jmbabazi <jmbabazi@pih.org>
2016-06-04 19:52:29 -0400

    IptablesInternetSharing.sh
    This script shares internet connection. This is moslty used on vpn child servers that need to send e-mails.

---
+++ IptablesInternetSharing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## Internet connection sharing script
+WAN=enxda1d7240ff16
+sysctl -w net.ipv4.ip_forward=1
+sysctl -p
+iptables -X
+iptables -F
+iptables -t nat -X
+iptables -t nat -F
+iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -I FORWARD  -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -I POSTROUTING -o $WAN -j MASQUERADE
